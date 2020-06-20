68beb249f9d3c1993ac0b4a4bbe0e5268ad70f4f
petrapavlis <petra.pavlis@gmail.com>
2020-06-20 19:18:43 +0200

    css update

--- index.js
+++ index.js
@@ -74,12 +74,35 @@ for (let i = 0; i < places.specPlaces.length; i += 1) {
   layer.addMarker(marker);
   let card = new SMap.Card();
   card.getHeader().innerHTML += `<strong>${places.specPlaces[i].name}</strong>`;
-  card.getBody().innerHTML += `Adresa: ${places.specPlaces[i].address}<br> Telefon: ${places.specPlaces[i].phoneNumber}<br> Email: ${places.specPlaces[i].email}<br> <a href="${places.specPlaces[i].web}" target="_blank">Přejdi na web</a>`;
+  card.getBody().innerHTML += `Adresa: ${places.specPlaces[i].address}<br> Telefon: ${places.specPlaces[i].phoneNumber}<br> E-mail: ${places.specPlaces[i].email}<br> <a href="${places.specPlaces[i].web}" target="_blank">Přejdi na web</a>`;
   marker.decorate(SMap.Marker.Feature.Card, card);
 }
 
 const cz = map.computeCenterZoom(coordsForZoom); //this method calculates the position of the map so that all the markers are visible at the same time
 map.setCenterZoom(cz[0], cz[1]);
+
+
+//cards
+const placeList = document.querySelector('.places');
+
+const renderPlace = (name, address, phone, email, web) => {
+  const placeElm = document.createElement("div")
+  placeElm.className = "spec-place"
+  placeElm.innerHTML = `
+   <div class="card">
+     <div class="place__name">${name}</div>
+     <div class="place__address">Adresa: ${address}</div>
+     <div class="episode__phone">Tel: ${phone}</div>
+     <div class="episode__email">E-mail: ${email}</div>
+     <div class="episode__web"><a href="${web}" target="_blank">Přejdi na web</a></div>
+   </div>`
+  return placeElm
+}
+
+for (let i = 0; i < places.specPlaces.length; i += 1) {
+  const placeElm = renderPlace(places.specPlaces[i].name, places.specPlaces[i].address, places.specPlaces[i].phoneNumber, places.specPlaces[i].email, places.specPlaces[i].web);
+  placeList.appendChild(placeElm);
+}
 
 
 
