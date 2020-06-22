b8ddaad18b83d0ec806c31dcfba3b53814b1fb30
Lenka Erbenova <lenka.erbenova79@gmail.com>
2020-06-22 20:47:30 +0200

    scrollovani a zmena paddingu dana pryc

--- js/otestuj_se.js
+++ js/otestuj_se.js
@@ -51,14 +51,3 @@ radio.forEach((input) => {
   }
   )
 })
-
-const prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  const currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-75px";
-  }
-  prevScrollpos = currentScrollPos;
-}
